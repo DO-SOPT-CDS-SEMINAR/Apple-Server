56a947e551f56ba0b8f5b18ab92ac3d3cef4e38b
ChaeAg <dydkglel123@gmail.com>
2023-11-21 02:34:35 +0900

    #3 [CORRECT] iPad Pro 악세서리 정보 조회 API의 Response 요소 이름 변경

--- src/main/java/org/sopthapse/www/HapseProject/dto/response/Accessory/AccessoryGetResponse.java
+++ src/main/java/org/sopthapse/www/HapseProject/dto/response/Accessory/AccessoryGetResponse.java
@@ -3,15 +3,15 @@ package org.sopthapse.www.HapseProject.dto.response.Accessory;
 import java.util.List;
 
 public record AccessoryGetResponse(
-        String name,
-        String cost,
-        List<String> imgUrls
+        String accessoryName,
+        String accessoryCost,
+        List<String> accessoryImgUrls
 ) {
-    public static AccessoryGetResponse of(String modelType, int cost, List<String> imgUrls) {
+    public static AccessoryGetResponse of(String modelType, int accessoryCost, List<String> accessoryImgUrls) {
         return new AccessoryGetResponse(
                 String.format("iPad Pro %s용 Magic Keyboard - 한국어 - 화이트", modelType),
-                String.format("%,d", cost),
-                imgUrls
+                String.format("%,d", accessoryCost),
+                accessoryImgUrls
         );
     }
 }
