6cb3770cb77e48d8b137f4a8aaa4de5ade977006
ChaeAg <dydkglel123@gmail.com>
2023-11-22 18:57:34 +0900

    [ADD] API 호출 시 에러가 발생할 경우 응답에 에러메세지를 담아 보내는 에러 응답 객체 구현

---
+++ src/main/java/org/sopthapse/www/HapseProject/common/exception/BadRequestException.java
@@ -0,0 +1,14 @@
+package org.sopthapse.www.HapseProject.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BadRequestException extends BaseException {
+
+    public BadRequestException() {
+        super(HttpStatus.BAD_REQUEST);
+    }
+
+    public BadRequestException(String responseMessage) {
+        super(HttpStatus.BAD_REQUEST, responseMessage);
+    }
+}
